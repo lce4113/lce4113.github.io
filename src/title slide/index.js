0140f7b7a7c6381373d3f1f8fd4f49e38d5196a8
Om Mahesh <2om.mahesh@gmail.com>
2022-06-09 17:51:27 -0700

    made favicon and open graph stuff

--- src/title slide/index.js
+++ src/title slide/index.js
@@ -1,9 +1,7 @@
-function TitleSlide() {
+export default function TitleSlide() {
   return (
-    <h1 className="font-bold underline">
+    <h1 className="bg-orange-50">
       Hello world!
     </h1>
   )
 }
-
-export default TitleSlide;
---
+++ src/title slide/wave.js
@@ -0,0 +1,5 @@
+export default function wave() {
+  return (
+    <canvas />
+  )
+};
