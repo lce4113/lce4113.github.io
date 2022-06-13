4c92dc9db049f6548946c0607c501d2e62140562
Om Mahesh <2om.mahesh@gmail.com>
2022-06-12 18:50:44 -0700

    icons are  c o o l  s w a g  now

--- src/title slide/Icon.js
+++ src/title slide/Icon.js
@@ -1,8 +1,9 @@
 export default function Icon(props) {
-  return <a href={props.link} target="_blank" rel="noopener noreferrer">
-    <div className="w-16 bg-white mx-2" />
-    <img
-      className="w-14 rounded-xl transition-transform hover:scale-125"
-      src={props.src} alt={props.alt} />;
+  return <a className="group relative w-16 h-16" href={props.link} target="_blank" rel="noopener noreferrer">
+    <div className="flex justify-center items-center absolute w-16 h-16 rounded-2xl bg-white transition-transform group-hover:scale-125">
+      <img
+        className="absolute w-14 rounded-xl transition-transform group-hover:scale-90"
+        src={props.src} alt={props.alt} />
+    </div>
   </a>
 }
--- src/title slide/index.js
+++ src/title slide/index.js
@@ -2,7 +2,7 @@ import Waves from "./Wave";
 import Icon from "./Icon";
 import discord from "../assets/discord.png";
 import github from "../assets/github.png";
-import gmail from "../assets/gmail.png";
+import email from "../assets/email.png";
 import instagram from "../assets/instagram.png";
 import linkedin from "../assets/linkedin.png";
 
@@ -10,12 +10,19 @@ export default function TitleSlide() {
   return (
     <figure className="flex flex-col w-screen h-screen items-center justify-center">
       <Waves />
-      <h1 contentEditable="true" className="text-10xl text-transparent bg-clip-text bg-gradient-to-r from-[#32FFAF] to-[#32AFFF] outline-none">Om Mahesh</h1>
-      <div className="flex">
+      <h1 contentEditable="true"
+        className="leading-tight text-10xl text-transparent bg-clip-text bg-gradient-to-r from-[#32FFAF] to-[#32AFFF] outline-none">
+        Om Mahesh
+      </h1>
+      <div className="flex items-center">
         <Icon src={discord} alt="Discord" link="https://discordapp.com/users/712836610139488346/"></Icon>
+        <div className="w-5 h-5 rounded-full bg-white mx-4"></div>
         <Icon src={github} alt="Github" link="https://github.com/lce4113/"></Icon>
-        <Icon src={gmail} alt="Gmail" link="mailto:2om.mahesh@gmail.com"></Icon>
+        <div className="w-5 h-5 rounded-full bg-white mx-4"></div>
+        <Icon src={email} alt="Email" link="mailto:2om.mahesh@gmail.com"></Icon>
+        <div className="w-5 h-5 rounded-full bg-white mx-4"></div>
         <Icon src={instagram} alt="Instagram" link="https://www.instagram.com/lce4113/"></Icon>
+        <div className="w-5 h-5 rounded-full bg-white mx-4"></div>
         <Icon src={linkedin} alt="LinkedIn" link="https://www.linkedin.com/in/om-mahesh-a76025224/"></Icon>
       </div>
     </figure>
